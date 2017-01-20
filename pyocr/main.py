ec7dad4e92e3d650bda9b514c557411361d9ca6e
David Sisson <davidsissonemail@gmail.com>
2017-01-19 20:42:37 -0800

    Added crop image, then save and OCR

--- pyocr/main.py
+++ pyocr/main.py
@@ -5,6 +5,17 @@ import sys
 sys.path.append("C:\\Program Files\\Tesseract-OCR")
 #image = Image.open("c:\\projects\\pyocr\\pyocr\\test2.jpg")
 #image = Image.open("c:\\projects\\pyocr\\pyocr\\test3.png")
-image = Image.open("c:\\projects\\pyocr\\pyocr\\test4.jpg")
+#image = Image.open("c:\\projects\\pyocr\\pyocr\\test2.jpg")
+#print(pytesseract.image_to_string(image))
 
-print(pytesseract.image_to_string(image))
+
+image = Image.open("c:\\projects\\pyocr\\pyocr\\test2.jpg")
+#(a, b, c, d)
+#upper left, lower right
+cropsection =  (455, 340, 545, 370)
+croppedimage = image.crop(cropsection)
+croppedimage.save("c:\\projects\\pyocr\\pyocr\\test22.jpg")
+croppedimage.show()
+
+image2 = Image.open("c:\\projects\\pyocr\\pyocr\\test22.jpg")
+print(pytesseract.image_to_string(image2))
