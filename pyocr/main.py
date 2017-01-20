f29909b002717e6cfed4364479bc5d75bacb6303
David Sisson <davidsissonemail@gmail.com>
2017-01-19 18:16:47 -0800

    Different tests including close up of the odometer

--- pyocr/main.py
+++ pyocr/main.py
@@ -3,6 +3,8 @@ import pytesseract
 import sys
 
 sys.path.append("C:\\Program Files\\Tesseract-OCR")
-image = Image.open("c:\\projects\\pyocr\\pyocr\\test2.jpg")
+#image = Image.open("c:\\projects\\pyocr\\pyocr\\test2.jpg")
+#image = Image.open("c:\\projects\\pyocr\\pyocr\\test3.png")
+image = Image.open("c:\\projects\\pyocr\\pyocr\\test4.jpg")
 
 print(pytesseract.image_to_string(image))
